ff33507eca9fa630df62176f50f3ebeb1f09c8aa
Manu Wallner <manu.wallner@me.com>
2015-05-09 18:05:59 +0200

    Initial Commit

---
+++ Yank/Yank.h
@@ -0,0 +1,13 @@
+//
+//  Yank.h
+//  Yank
+//
+//  Created by Manu Wallner on 09/05/15.
+//  Copyright (c) 2015 Manu Wallner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Yank : NSObject
+
+@end
---
+++ Yank/Yank.m
@@ -0,0 +1,13 @@
+//
+//  Yank.m
+//  Yank
+//
+//  Created by Manu Wallner on 09/05/15.
+//  Copyright (c) 2015 Manu Wallner. All rights reserved.
+//
+
+#import "Yank.h"
+
+@implementation Yank
+
+@end
